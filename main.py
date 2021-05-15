f64f7567a6b91ac711e2528e44630d81510831da
Hamza Haruna MOHAMMED <hamzasharoon@gmail.com>
2021-05-15 23:58:38 +0300

    updates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import nodeMeasures
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # run the nodeMeasures.py script
-    network_node_measures = nodeMeasures()
+    nodeMeasures.nodeMeasures()
 
     print('Done..')
     sys.exit()
